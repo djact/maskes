9c9f251417eb3676811c8a785bd03a14ec8325d0
pianote <pianote@gmail.com>
2020-08-29 11:28:13 -0700

    move middlewares to project folder

--- maskes/settings.py
+++ maskes/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'requests.middleware.AdminTimezoneMiddleware',
+    'maskes.middleware.AdminTimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'maskes.urls'
