17cf822b1f8b25019bde4a9d50c20014ce55cfc4
Duc Vo <61364158+pianote@users.noreply.github.com>
2020-11-12 19:18:51 -0800

    Duc add supporter payment info to reimbursement (#37)
    * add supporter vemo to reimbursement
    
    * add link to support from reimbursement

--- funds/admin.py
+++ funds/admin.py
@@ -5,12 +5,12 @@ from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 class ReimbursementAdmin(admin.ModelAdmin):
-    list_display = ('id','volunteer', 'request', 'supporter', 'total_cost', 'amount', 'status','created_date')
+    list_display = ('id','volunteer_link', 'request', 'supporter', 'total_cost', 'amount', 'status','created_date')
     list_display_links = ('id',)
     list_filter = ('status',)
     list_editable = ('status',)
     search_fields = ('id','volunteer__id',)
-    readonly_fields = ('request','created_date')
+    readonly_fields = ('request','created_date', 'supporter','volunteer_link','supporter_venmo')
     list_per_page = 25
 
     def request(self, obj):
@@ -22,10 +22,24 @@ class ReimbursementAdmin(admin.ModelAdmin):
 
     def supporter(self, obj):
         return mark_safe('<a href="{}">{}</a>'.format(
-            reverse('admin:%s_%s_change' % (obj.volunteer.supporter._meta.app_label,  obj.volunteer.supporter._meta.model_name),  args=[obj.volunteer.supporter.id] ),
+            reverse('admin:%s_%s_change' % (obj.volunteer.supporter.userprofile._meta.app_label,  obj.volunteer.supporter.userprofile._meta.model_name),  args=[obj.volunteer.supporter.userprofile.id] ),
             obj.volunteer.supporter
         ))
     supporter.short_description = 'Supporter Info'
+
+    def supporter_venmo(self, obj):
+        return mark_safe('<a href="{}"> {}</a>'.format(
+            'https://venmo.com/{}'.format(obj.volunteer.supporter.userprofile.venmo),
+            obj.volunteer.supporter.userprofile.venmo,
+        ))
+    supporter_venmo.short_description = 'Supporter Venmo'
+
+    def volunteer_link(self, obj):
+        return mark_safe('<a href="{}">{}</a>'.format(
+            reverse('admin:%s_%s_change' % (obj.volunteer._meta.app_label,  obj.volunteer._meta.model_name),  args=[obj.volunteer.id] ),
+            obj.volunteer
+        ))
+    volunteer_link.short_description = 'Support Info'
 
     
 admin.site.register(Reimbursement, ReimbursementAdmin);
