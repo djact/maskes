4637e1365146f0b1a5ea0f43ae38b25ffcd11984
Duc Vo <61364158+pianote@users.noreply.github.com>
2021-02-11 15:06:27 -0800

    Amazon s3 dev (#36)
    * amazon S3 setup
    
    * use s3 for production only
    
    * reszing before upload to AWS S3 Bucket
    
    * update readme.md for dev env setup

--- funds/models.py
+++ funds/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.contrib.auth import get_user_model
 from PIL import Image
 from supports.models import Volunteer, Request
+from io import BytesIO
+from django.core.files import File
+from pathlib import Path
 
 User = get_user_model()
 
@@ -14,14 +17,30 @@ class Reimbursement(models.Model):
     status = models.CharField(choices=(('In Process', 'In Process'),('Completed','Completed')), max_length=150, default=None)
     volunteer_notes = models.TextField(blank=True, null=True, max_length=1024)
     created_date = models.DateTimeField(default=timezone.now)
-    
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        img = Image.open(self.receipt_photo.path)       
+        img = Image.open(self.receipt_photo)       
         if img.height > 500 or img.width > 500:
+            image_types = {
+                'jpeg': 'JPEG',
+                'jpg': 'JPEG',
+                'png': 'PNG',
+                'gif': 'GIF',
+                'tif': 'TIFF',
+            }
+            buffer = BytesIO()
+            image_path = Path(self.receipt_photo.name)
+            image_filename = image_path.name
+            filename_suffix = image_path.suffix[1:].lower()
+            image_format = image_types[filename_suffix] 
             output_size = (500, 500)
             img.thumbnail(output_size)
-            img.save(self.receipt_photo.path, quality=75)
+            img.save(buffer, format=image_format)
+            file_object = File(buffer)
+            file_object.content_type = 'image/jpeg'
+            self.receipt_photo.save('{}-{}'.format(self.id, image_filename), file_object)
+            self.save()
     
     def __str__(self):
         return '{} - {} - ${}'.format(self.id, self.volunteer.supporter.display_name, self.amount);
